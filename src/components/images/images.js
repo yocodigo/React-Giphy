dddaf2e7f49d2e304659bbc86d7f8eeecd3b744b
unknown <jjhernandez94@yahoo.com>
2018-05-04 00:42:19 -0700

    Attempting to set state with the response from API call to Giphy.

--- src/components/images/images.js
+++ src/components/images/images.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Image } from 'semantic-ui-react';
+import { Image, Grid } from 'semantic-ui-react';
 import styles from './styles.js';
 
 const ImageExampleCircular = () => (
-
-  <Image alt='image' style={styles} src={require('./bali.png')} size='medium' as='a'
-  href='http://google.com'
-  target='_blank'
-  rounded /> 
-  
-
+  <Grid.Column> 
+    <Image alt='image' style={styles} src={require('./bali.png')} size='medium' as='a'
+    href='http://google.com'
+    target='_blank'
+    rounded /> 
+  </Grid.Column>
 )
 
 export default ImageExampleCircular;
